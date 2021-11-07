a67ec60c1f61f6f3f4d85af09b2bbf2281a471a6
Lukas <lukas.ikvm1@gmail.com>
2021-11-07 13:51:35 +0100

    added export to jar file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,13 @@
 module se.iths.javafx.colorpicker.colorpicker {
     requires javafx.controls;
+    requires javafx.base;
     requires javafx.fxml;
+    requires javafx.graphics;
+
 
     opens se.iths.javafx.colorpicker.colorpicker to javafx.fxml;
     exports se.iths.javafx.colorpicker.colorpicker;
-    exports se.iths.javafx.colorpicker.colorpicker.Shapes;
     opens se.iths.javafx.colorpicker.colorpicker.Shapes to javafx.fxml;
+    exports se.iths.javafx.colorpicker.colorpicker.Shapes;
+
 }
---
+++ src/main/java/se/iths/javafx/colorpicker/colorpicker/Main.java
@@ -0,0 +1,7 @@
+package se.iths.javafx.colorpicker.colorpicker;
+
+public class Main {
+    public static void main(String[] args) {
+        HelloApplication.main(args);
+    }
+}
